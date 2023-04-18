048cfb4b29819bffa7f928884d357738253935ec
texor90 <rudzinski.jacek@icloud.com>
2023-04-18 19:28:52 +0200

    cofiguration

---
+++ SA/src/test/java/com/sa/jacek/sa/SaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.jacek.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
